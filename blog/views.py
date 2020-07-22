bc96ac70ce286d67eac821b439bc641e5ca056e2
ardiyan15 <ardhiyan15@gmail.com>
2020-07-22 21:55:59 +0700

    create django forms

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
+from .forms import PostForm
+from django.utils import timezone
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.all()
@@ -9,3 +12,16 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     comment = Comment.objects.filter(post_id__id=pk)
     return render(request, 'blog/post_detail.html', {'post': post, 'comment': comment})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
